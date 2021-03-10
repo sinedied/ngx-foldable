e03a96521b06075167abaac92cf25d6a312c7e85
sinedied <noda@free.fr>
2021-03-10 12:23:13 +0100

    test: add unit tests

--- projects/lib/src/core/if-span.directive.spec.ts
+++ projects/lib/src/core/if-span.directive.spec.ts
@@ -1,8 +1,150 @@
-import { IfSpanDirective } from './if-span.directive';
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Subject } from 'rxjs';
+import { IfSpanDirective, SpanCondition } from './if-span.directive';
+import { ScreenContext, ScreenContextData } from './screen-context';
+import { ScreenSpanning } from './screen-spanning';
+
+@Component({
+  selector: 'test',
+  template: `<div *fdIfSpan="condition">visible</div>`,
+})
+class TestComponent {
+  condition = SpanCondition.None;
+}
+
+const getter = (obj, prop): jasmine.Spy =>
+  Object.getOwnPropertyDescriptor(obj, prop).get as jasmine.Spy;
 
 describe('IfSpanDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IfSpanDirective();
-    expect(directive).toBeTruthy();
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let screenContextSpy: jasmine.SpyObj<ScreenContext>;
+  let el: DebugElement;
+
+  beforeEach(() => {
+    const fakeObservable$ = new Subject<ScreenContextData>();
+    screenContextSpy = jasmine.createSpyObj(
+      'ScreenContext',
+      ['asObservable', 'asObject'],
+      ['isMultiScreen', 'screenSpanning', 'windowSegments']
+    );
+    screenContextSpy.asObservable.and.returnValue(fakeObservable$);
+    getter(screenContextSpy, 'isMultiScreen').and.returnValue(false);
+    getter(screenContextSpy, 'screenSpanning').and.returnValue(ScreenSpanning.None);
+
+    fixture = TestBed.configureTestingModule({
+      declarations: [IfSpanDirective, TestComponent],
+      providers: [{ provide: ScreenContext, useValue: screenContextSpy }],
+    }).createComponent(TestComponent);
+
+    component = fixture.componentInstance;
+  });
+
+  describe('condition None', () => {
+    it('should make el visible on single screen mode', () => {
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('div'));
+
+      expect(el).not.toBeNull();
+    });
+
+    it('should make el invisible on multi screen mode', () => {
+      getter(screenContextSpy, 'isMultiScreen').and.returnValue(true);
+      getter(screenContextSpy, 'screenSpanning').and.returnValue(ScreenSpanning.Vertical);
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('div'));
+  
+      expect(el).toBeNull();
+    });
+  });
+
+  describe('condition Multi', () => {
+    beforeEach(() => {
+      component.condition = SpanCondition.Multi;
+      getter(screenContextSpy, 'screenSpanning').and.returnValue(ScreenSpanning.Horizontal);
+    });
+
+    it('should make el visible on multi screen mode', () => {
+      getter(screenContextSpy, 'isMultiScreen').and.returnValue(true);
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('div'));
+
+      expect(el).not.toBeNull();
+    });
+
+    it('should make el invisible on single screen mode', () => {
+      getter(screenContextSpy, 'isMultiScreen').and.returnValue(false);
+      getter(screenContextSpy, 'screenSpanning').and.returnValue(ScreenSpanning.None);
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('div'));
+
+      expect(el).toBeNull();
+    });
+  });
+
+  describe('condition Horizontal', () => {
+    beforeEach(() => {
+      component.condition = SpanCondition.Horizontal;
+      getter(screenContextSpy, 'isMultiScreen').and.returnValue(true);
+    });
+
+    it('should make el visible on horizontal span mode', () => {
+      getter(screenContextSpy, 'screenSpanning').and.returnValue(ScreenSpanning.Horizontal);
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('div'));
+
+      expect(el).not.toBeNull();
+    });
+
+    it('should make el invisible on vertical span mode', () => {
+      getter(screenContextSpy, 'screenSpanning').and.returnValue(ScreenSpanning.Vertical);
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('div'));
+
+      expect(el).toBeNull();
+    });
+
+    it('should make el invisible on single screen mode', () => {
+      getter(screenContextSpy, 'isMultiScreen').and.returnValue(false);
+      getter(screenContextSpy, 'screenSpanning').and.returnValue(ScreenSpanning.None);
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('div'));
+
+      expect(el).toBeNull();
+    });
+  });
+
+  describe('condition Vertical', () => {
+    beforeEach(() => {
+      component.condition = SpanCondition.Vertical;
+      getter(screenContextSpy, 'isMultiScreen').and.returnValue(true);
+    });
+
+    it('should make el invisible on horizontal span mode', () => {
+      getter(screenContextSpy, 'screenSpanning').and.returnValue(ScreenSpanning.Horizontal);
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('div'));
+
+      expect(el).toBeNull();
+    });
+
+    it('should make el visible on vertical span mode', () => {
+      getter(screenContextSpy, 'screenSpanning').and.returnValue(ScreenSpanning.Vertical);
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('div'));
+
+      expect(el).not.toBeNull();
+    });
+
+    it('should make el invisible on single screen mode', () => {
+      getter(screenContextSpy, 'isMultiScreen').and.returnValue(false);
+      getter(screenContextSpy, 'screenSpanning').and.returnValue(ScreenSpanning.None);
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('div'));
+
+      expect(el).toBeNull();
+    });
   });
 });
--- projects/lib/src/core/split-layout.directive.spec.ts
+++ projects/lib/src/core/split-layout.directive.spec.ts
@@ -1,8 +1,64 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Subject } from 'rxjs';
+import { ScreenContext, ScreenContextData } from './screen-context';
+import { ScreenSpanning } from './screen-spanning';
 import { SplitLayoutDirective } from './split-layout.directive';
 
+@Component({
+  template: `<div fdSplitLayout="flex"></div>`,
+})
+class TestComponent {}
+
+const getter = (obj, prop): jasmine.Spy =>
+  Object.getOwnPropertyDescriptor(obj, prop).get as jasmine.Spy;
+
 describe('SplitLayoutDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SplitLayoutDirective();
-    expect(directive).toBeTruthy();
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let screenContextSpy: jasmine.SpyObj<ScreenContext>;
+  let fakeObservable$: Subject<ScreenContextData>;
+  let el: DebugElement;
+
+  beforeEach(() => {
+    fakeObservable$ = new Subject<ScreenContextData>();
+    screenContextSpy = jasmine.createSpyObj(
+      'ScreenContext',
+      ['asObservable', 'asObject'],
+      ['isMultiScreen', 'screenSpanning', 'windowSegments']
+    );
+    screenContextSpy.asObservable.and.returnValue(fakeObservable$);
+    getter(screenContextSpy, 'isMultiScreen').and.returnValue(false);
+    getter(screenContextSpy, 'screenSpanning').and.returnValue(
+      ScreenSpanning.None
+    );
+
+    fixture = TestBed.configureTestingModule({
+      declarations: [SplitLayoutDirective, TestComponent],
+      providers: [{ provide: ScreenContext, useValue: screenContextSpy }],
+    }).createComponent(TestComponent);
+
+    fakeObservable$.next();
+    component = fixture.componentInstance;
+  });
+
+  it('should not add any css in single screen mode', () => {
+    fixture.detectChanges();
+    el = fixture.debugElement.query(By.css('div'));
+
+    expect(el.nativeElement.getAttribute('style')).toBeNull();
+  });
+
+  it('should add styling in multi screen mode', () => {
+    getter(screenContextSpy, 'isMultiScreen').and.returnValue(true);
+    getter(screenContextSpy, 'screenSpanning').and.returnValue(
+      ScreenSpanning.Horizontal
+    );
+    fakeObservable$.next();
+    fixture.detectChanges();
+    el = fixture.debugElement.query(By.css('div'));
+
+    expect(el.nativeElement.getAttribute('style')).not.toBeNull();
   });
 });
