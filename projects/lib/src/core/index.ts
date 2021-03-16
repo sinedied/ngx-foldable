bf221b2ce760f43249799a83b64aaab63a9df9b2
sinedied <noda@free.fr>
2021-03-16 15:27:20 +0100

    refactor: allow live reload on lib

---
+++ projects/lib/src/core/index.ts
@@ -0,0 +1,7 @@
+export * from './screen-context';
+export * from './screen-spanning';
+export * from './media-queries';
+export * from './split-layout.directive';
+export * from './window.directive';
+export * from './if-span.directive';
+export * from './foldable.module';
