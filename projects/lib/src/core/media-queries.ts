fc26aae9ab035715c8e429cc4aee5b7250f9b19f
sinedied <noda@free.fr>
2021-11-22 14:23:27 +0100

    feat: update media queries to support new css segments API

--- projects/lib/src/core/media-queries.ts
+++ projects/lib/src/core/media-queries.ts
@@ -1,4 +1,4 @@
 /** Media query used to detect single fold horizontal screen mode. */
-export const singleFoldHorizontal = '(screen-spanning: single-fold-horizontal)';
+export const singleFoldHorizontal = '(screen-spanning: single-fold-horizontal), (vertical-viewport-segments: 2)';
 /** Media query used to detect single fold vertical screen mode. */
-export const singleFoldVertical = '(screen-spanning: single-fold-vertical)';
+export const singleFoldVertical = '(screen-spanning: single-fold-vertical), (horizontal-viewport-segments: 2)';
